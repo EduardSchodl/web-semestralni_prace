a16b757a5c6b6f262e3e254d3e789aab8899d0fb
Eduard Schodl <schodle@spseplzen.cz>
2024-09-29 22:50:35 +0200

    article detail base structure

--- src/Controllers/ArticleController.php
+++ src/Controllers/ArticleController.php
@@ -1,8 +1,13 @@
 <?php
     namespace Web\Project\Controllers;
 
+    use Web\Project\Models\ArticleModel;
+
     class ArticleController extends BaseController{
         function index($data = []){
-            $this->render("ArticleView.twig", ["title" => $data["title"]]);
+            $db = new ArticleModel();
+            $article = $db->getArticle($data["params"][0]);
+
+            $this->render("ArticleView.twig", ["title" => $data["title"], "article" => $article]);
         }
     }
