f426237205d77ae68cae799060a0f3297a232724
Eduard Schodl <schodle@spseplzen.cz>
2024-09-30 22:52:10 +0200

    ckeditor ready to implement

--- src/Models/ArticleModel.php
+++ src/Models/ArticleModel.php
@@ -13,7 +13,7 @@
         function getArticle($id){
             $pdo = self::getConnection();
 
-            $stmt = $pdo->prepare("SELECT articles.*, users.first_name, users.last_name, users.email FROM articles INNER JOIN users ON articles.author_id = users.id_user WHERE id_article=:id");
+            $stmt = $pdo->prepare("SELECT articles.*, users.id_user, users.first_name, users.last_name, users.email FROM articles INNER JOIN users ON articles.author_id = users.id_user WHERE id_article=:id");
             $stmt->execute(["id" => $id]);
             return $stmt->fetch(\PDO::FETCH_ASSOC);
         }
